807492812b5dd50d5638f2653f8b72b597a420eb
linlookfor <linlookfor@gmail.com>
2013-09-05 20:34:13 +0800

    liner solution

--- Array/Array/Array.cpp
+++ Array/Array/Array.cpp
@@ -6,16 +6,26 @@
 #include <iostream>
 using namespace std;
 int get_max_length(vector<int> &A);
+void printArray(vector<int> &A);
+int get_max_length_liner_time(vector<int> &A);
 int _tmain(int argc, _TCHAR* argv[])
 {
 	vector<int> A;
-	A.push_back(6);
-	A.push_back(4);
-	A.push_back(7);
-	A.push_back(8);
-	A.push_back(3);
-	A.push_back(2);
-	A.push_back(5);
+    A.push_back(6);
+    A.push_back(4);
+    A.push_back(7);
+    A.push_back(8);
+    A.push_back(3);
+    A.push_back(2);
+    A.push_back(5);
+
+    printArray(A);
+	cout<<get_max_length(A)<<endl;
+    cout<<get_max_length_liner_time(A)<<endl;
+	return 0;
+}
+void printArray(vector<int> &A)
+{
     cout<< "Array:";
     for(vector<int>::iterator it=A.begin();it!=A.end();it++)
     {
@@ -23,10 +33,7 @@ int _tmain(int argc, _TCHAR* argv[])
         cout<<" ";
     }
     cout<<endl;
-	cout<<get_max_length(A)<<endl;
-	return 0;
 }
-
 int get_max_length(vector<int> &A)
 {
 	int n = A.size();
@@ -46,4 +53,42 @@ int get_max_length(vector<int> &A)
 		}
 	}
 	return max_length;
+}
+
+int get_max_length_liner_time(vector<int> &A)
+{
+    int n = A.size();
+    vector<int> left_max_index(n,0);
+    left_max_index[n-1] = n-1;
+    for(int i = n-2;i>=0;i--)
+    {
+        if(A[i] <= A[left_max_index[i+1]])
+        {
+            left_max_index[i] = left_max_index[i+1];
+        }
+        else
+        {
+            left_max_index[i] = i;
+        }
+    }
+    cout<<"left_max_index ";
+    printArray(left_max_index);
+    int i=0,j=0,max_length = 0;
+    while(j<n)
+    {
+        j = left_max_index[j];
+        if(A[j]>=A[i])
+        {
+            if(j - i > max_length)
+            {
+                max_length = j - i;
+            }
+            j++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return max_length;
 }
