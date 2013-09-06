e69b68df46417d1352b0025d2d15f6754e708615
linlookfor <linlookfor@gmail.com>
2013-09-06 12:34:01 +0800

    Update MaxSum.cpp

--- Array/MaxSum/MaxSum/MaxSum.cpp
+++ Array/MaxSum/MaxSum/MaxSum.cpp
@@ -1,11 +1,37 @@
-// MaxSum.cpp : 定义控制台应用程序的入口点。
+// MaxSum.cpp : 锟斤拷锟斤拷锟斤拷锟斤拷台应锟矫筹拷锟斤拷锟斤拷锟斤拷锟节点。
 //
 
 #include "stdafx.h"
+#include <vector>
+#include <iostream>
+using namespace std;
+#typedef vector<int> IntArray
 
+int maxSum(IntArray &A);
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	IntArray A;
+	A.push_back(5);
+	A.push_back(3);
+	A.push_back(-1);
+	A.push_back(7);
+	A.push_back(0);
 	return 0;
 }
 
+int maxSum(IntArray &A)
+{
+	int n = A.size();
+	if(n <= 0)
+		return 0;
+	IntArray max_sum(n+1,0);//max_sum[i] : maxSum of first i element
+	max_sum[1] = A[0] > max_sum[0] ? A[0] : max_sum[0]; 
+	for(int i=1;i<n;i++)
+	{
+		int sum_with_i = max_sum[i-1] + A[i];
+		int sum_without_i = max_sum[i];
+		max_sum[i+1] = max(sum_with_i,sum_without_i);
+	}
+	return max_sum[n];
+}
